0b08c2471d0ba4b3c56df9c3a9ad710da22e8e78
AZelikov <andrey.zelikay@gmail.com>
2020-06-08 16:23:07 +0300

    task6

--- task6/src/org/epam/task6/Main.java
+++ task6/src/org/epam/task6/Main.java
@@ -1,34 +1,39 @@
 package org.epam.task6;
 
-import java.math.BigInteger;
-
 public class Main {
-
-    private static final int INTEGER_INFINITY = (int) Double.POSITIVE_INFINITY;
 
     public static void main(String[] args) {
         int firstElement = 12;
         int step = -30;
 
-        long currentSum = firstElement;
-        BigInteger n = BigInteger.ZERO;
-
-        while (currentSum <= INTEGER_INFINITY && currentSum >= -INTEGER_INFINITY) {
-            currentSum += firstElement + n.multiply(BigInteger.valueOf(step)).longValue();
-            n = n.add(BigInteger.ONE);
-        }
-
-        System.out.println("Integer overflow: " + n);
-
-        while (true) {
-            try {
-                currentSum = Math.addExact(currentSum, Math.addExact(firstElement, n.multiply(BigInteger.valueOf(step)).longValue()));
-                n = n.add(BigInteger.ONE);
-            } catch (ArithmeticException e) {
-                n = n.add(BigInteger.ONE);
-                System.out.println("Long overflow: " + n);
-                break;
-            }
-        }
+        System.out.println("int overflow " + findIntOverflow(firstElement, step));
+        System.out.println("long overflow " + findLongOverflow(firstElement, step));
     }
+
+    public static int findIntOverflow(int firstElement, int step) {
+        int sum = firstElement;
+        int n = 1;
+
+        while((step < 0 && sum <= firstElement) ||
+                (step > 0 && sum >= firstElement)) {
+            sum += firstElement + n * step;
+            n++;
+        }
+
+        return n;
+    }
+
+    public static long findLongOverflow(int firstElement, int step) {
+        long sum = firstElement;
+        long n = 1;
+
+        while((step < 0 && sum <= firstElement) ||
+                (step > 0 && sum >= firstElement)) {
+            sum += firstElement + n * step;
+            n++;
+        }
+
+        return n;
+    }
+
 }
