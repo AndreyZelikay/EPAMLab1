d81da2106a1eaed946e0b58193a8858ba96bce1d
AZelikov <andrey.zelikay@gmail.com>
2020-06-08 15:40:42 +0300

    task1 updated

--- task1/src/org/epam/task1/main/Main.java
+++ task1/src/org/epam/task1/main/Main.java
@@ -1,31 +1,14 @@
 package org.epam.task1.main;
 
-import java.math.BigDecimal;
-import java.math.MathContext;
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.err.println("Input a");
-
-        BigDecimal a = scanner.nextBigDecimal();
-
-        System.err.println("Input b");
-
-        BigDecimal b = scanner.nextBigDecimal();
-
-        System.err.println("Input c");
-
-        BigDecimal c = scanner.nextBigDecimal();
-
-        System.err.println("Input precision");
-
-        int precision = scanner.nextInt();
+        double a = 10;
+        double b = 5;
+        double c = 7;
 
         try {
-            BigDecimal result = solveEquation(a, b, c, precision);
+            double result = calcExpression(a, b, c);
 
             System.err.print("Result " + result);
         } catch (IllegalArgumentException e) {
@@ -33,22 +16,17 @@ public class Main {
         }
     }
 
-    private static BigDecimal solveEquation(BigDecimal a, BigDecimal b, BigDecimal c, int precision) {
-        BigDecimal valueUnderRoot = b.pow(2).add(BigDecimal.valueOf(4).multiply(a).multiply(c));
+    private static double calcExpression(double a, double b, double c) {
+        double valueUnderRoot = Math.pow(b, 2) + 4 * a * c;
 
-        if(valueUnderRoot.signum() < 0) {
+        if(valueUnderRoot < 0) {
             throw new IllegalArgumentException("Incorrect input: value under square root is lower then 0");
         }
 
-        if(a.signum() == 0 || b.signum() == 0) {
+        if(a == 0 || b == 0) {
             throw new IllegalArgumentException("Incorrect input: denominator is equal to 0");
         }
 
-        MathContext mc = new MathContext(precision);
-
-        return ((b.add(valueUnderRoot.sqrt(mc)))
-                .divide(BigDecimal.valueOf(2).multiply(a), mc))
-                .subtract(a.pow(3).multiply(c))
-                .add(BigDecimal.ONE.divide(b.pow(2), mc));
+        return (b + Math.sqrt(valueUnderRoot)) / (2 * a) - Math.pow(a, 3) * c + Math.pow(b, -2);
     }
 }
