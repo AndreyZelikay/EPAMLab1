4470632622f0d2a22e910ac1264b51185989de5b
AZelikov <andrey.zelikay@gmail.com>
2020-06-09 22:48:57 +0300

    task2

--- task2/src/org/epam/task2/Main.java
+++ task2/src/org/epam/task2/Main.java
@@ -3,10 +3,46 @@ package org.epam.task2;
 public class Main {
 
     public static void main(String[] args) {
-        int dayNumber = 265;
+        int dayNumber = 1;
 
-        CustomCalendar calendar = new CustomCalendar(dayNumber);
+        System.err.println(getDayAndMonth(dayNumber));
+    }
 
-        System.err.println(calendar.getDayInMonth() + " " + calendar.getMonthName());
+    public static DayAndMonth getDayAndMonth(int dayNumberInYear) {
+        if(dayNumberInYear <= 0) {
+            throw new IllegalArgumentException("illegal day number");
+        }
+
+        int monthNumber = 1;
+        int countedDayNumber = 1;
+        int numberDaysInMonth = 31;
+
+        while (countedDayNumber + numberDaysInMonth <= dayNumberInYear) {
+            countedDayNumber += numberDaysInMonth;
+            monthNumber++;
+
+            switch (monthNumber) {
+                case 1, 3, 5, 7, 8, 10, 12 ->numberDaysInMonth = 31;
+                case 4, 6, 9, 11 -> numberDaysInMonth = 30;
+                case 2 -> numberDaysInMonth = 28;
+            }
+        }
+
+        return new DayAndMonth(monthNumber, dayNumberInYear - countedDayNumber + 1);
+    }
+
+    private static class DayAndMonth {
+        public final int monthNumber;
+        public final int dayNumber;
+
+        public DayAndMonth(int monthNumber, int dayNumber) {
+            this.monthNumber = monthNumber;
+            this.dayNumber = dayNumber;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("month: %d day: %d", monthNumber, dayNumber);
+        }
     }
 }
